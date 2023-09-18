9009a2a29a48783c8b664ff052e671a2cb639f2d
Volodymyr Panov <vpanov25@gmail.com>
2023-09-18 22:17:02 +0200

    Created app.py for displaying model result

--- app.py
+++ app.py
@@ -1,33 +1,60 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 import os
 import tensorflow as tf
+from werkzeug.utils import secure_filename
 from classifier import classify
 
 app = Flask(__name__)
-
 STATIC_FOLDER = "static"
 UPLOAD_FOLDER = "static/uploads/"
 
-cnn_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "!!!!!")  # TODO Write file name for the model
+# Define allowed file extensions
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+cnn_model = tf.keras.models.load_model(os.path.join(STATIC_FOLDER, "models", "sport_balls_model.h5"))
+
+# Function to check if the file extension is allowed
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("base.html")
 
 
-@app.post("/classify")
-def upload_file():
+@app.route("/classify", methods=["POST"])
+def classify_image():
+    if "image" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
     file = request.files["image"]
-    upload_image_file = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(upload_image_file)
 
-    label = classify(cnn_model, upload_image_file)
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
 
-    return {
-        "label": label,
-    }
+    if file and allowed_file(file.filename):
+        # Securely generate a filename and save the file
+        filename = secure_filename(file.filename)
+        upload_image_file = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(upload_image_file)
+
+        # Classify the uploaded image
+        result, probability = classify(cnn_model, upload_image_file)
+
+        # Check if the result is a tuple
+        if isinstance(result, tuple) and len(result) > 0:
+            label = result[0]
+        else:
+            label = result
+
+        # If label is a tuple or list, convert it to a list
+        if isinstance(label, tuple):
+            label = list(label)
+        return render_template("result.html", label=label, probability=probability)
+    else:
+        return jsonify({"error": "Invalid file format"}), 400
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+from flask import json
 
 IMAGE_SIZE = (100, 100)
 
@@ -23,6 +23,23 @@ def classify(model, image_path: str):
     preprocessed_image = load_and_preprocess_data(image_path)
 
     predictions = model.predict(preprocessed_image)
-    score = predictions[0]
+    score = predictions[0].tolist()  # Convert the NumPy array to a Python list
 
-    return score
+    # Finding the index of the class with the highest probability
+    predicted_class_index = score.index(max(score))
+
+    class_labels = [
+        "American football", "Baseball", "Basketball", "Billiard", "Bowling",
+        "Cricket", "Football", "Golf", "Hockey ball", "Hockey puck", "Rugby",
+        "Shuttlecock", "Table tennis", "Tennis", "Volleyball",
+    ]
+    predicted_class = class_labels[predicted_class_index]
+
+    # Construct a JSON object with the predicted class and its corresponding probability
+    result = {
+        "Predicted Class": predicted_class,
+        "Probability": f"{round(max(score), 4) * 100} %"
+    }
+    # Convert the result to a JSON string
+    result_json = json.dumps(result)
+    return predicted_class, result["Probability"]
